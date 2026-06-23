2e874d0b8f06cc838f3f9a19806e10750b35331f
Naputt1 <naputtalt1@gmail.com>
2026-06-24 00:49:41 +0700

    move view to sample project

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,7 @@ export default defineConfig({
       "@boredgame/core": resolve(__dirname, "src/core/index.ts"),
       "@boredgame/engine": resolve(__dirname, "src/engine/index.ts"),
       "@boredgame/schemas": resolve(__dirname, "src/schemas/index.ts"),
-      "@boredgame/transport": resolve(__dirname, "src/transport/index.ts"),
-      "@boredgame/view": resolve(__dirname, "src/view/index.ts"),
-      "@boredgame/platform": resolve(__dirname, "src/platform/index.ts")
+      "@boredgame/transport": resolve(__dirname, "src/transport/index.ts")
     }
   }
 });
